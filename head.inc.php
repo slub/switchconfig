0cc5b749fe6420bbf30a751539e371454f870a24
Georg Sieber <Georg.Sieber@slub-dresden.de>
2020-05-08 10:16:49 +0200

    remove obsolete css include

--- head.inc.php
+++ head.inc.php
@@ -2,9 +2,6 @@
 <link rel='apple-touch-icon' sizes='128x128' href='img/favicon.png'>
 <link rel='stylesheet' type='text/css' href='style/font.css'>
 <link rel='stylesheet' type='text/css' href='style/main.css'>
-<?php if(isset($_COOKIE['design']) && $_COOKIE['design'] == 'dark') { ?>
-	<link rel='stylesheet' type='text/css' href='style/main.dark.css'>
-<?php } ?>
 <link rel='stylesheet' type='text/css' href='style/infobox.css'>
 <script language='javascript' type='text/javascript' src='js/main.js'></script>
 <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
