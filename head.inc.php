4d8b97145ec27364652d75f379746425780aff38
Georg Sieber <Georg.Sieber@slub-dresden.de>
2023-06-29 21:23:40 +0200

    remove unused JS

--- head.inc.php
+++ head.inc.php
@@ -5,7 +5,5 @@
 <link rel='stylesheet' type='text/css' href='webdesign-template/css/main.css'>
 <link rel='stylesheet' type='text/css' href='css/switchconfig.css'>
 
-<script language='javascript' type='text/javascript' src='js/main.js'></script>
-
 <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
 <meta name='author' content='Georg Sieber'>
